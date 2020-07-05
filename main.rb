b151037f3439ecd09a9fd55c1e9b1a0781e0756b
Gerhard Koekemoer <koekies19@gmail.com>
2020-07-05 20:22:57 +0200

    Add Tile replacement images

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require "./calc_avg_rgb.rb"
 panda = ImageList.new("Input Images/panda-4k.jpg")
 slice_image(panda, 20)
 
-rgb_values_for_rgb_images_folder = avg_rgb_for_folder("RGB Images")
+rgb_values_for_rgb_images_folder = avg_rgb_for_folder("Tile Replacement Images")
 rgb_values_for_tile_images_folder = avg_rgb_for_folder("Input Tile Images")
 
 print rgb_values_for_rgb_images_folder
